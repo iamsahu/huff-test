55c91bc1b371ae1f14e90e6e030be3a802f772d2
iamsahu <praffulsahu@gmail.com>
2022-10-14 11:53:20 +0200

    feat: decimals test working

--- test/ERC20.t.sol
+++ test/ERC20.t.sol
@@ -4,13 +4,27 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
+import "yield-utils-v2/token/IERC20Metadata.sol";
 
-contract SimpleStoreTest is Test {
+contract ERC20Test is Test {
     /// @dev Address of the SimpleStore contract.
-    SimpleStore public simpleStore;
+    IERC20Metadata public erc20;
 
     /// @dev Setup the testing environment.
     function setUp() public {
-        // simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+        address simpleERC20 = HuffDeployer.config().with_args(abi.encode(uint8(0x12))).deploy("SimpleERC20");
+        erc20 = IERC20Metadata(simpleERC20);
+    }
+
+    function testDecimals() public {
+        assertEq(erc20.decimals(),uint8(0x12));
+    }
+
+    // function testSymbol() public {
+    //     erc20.symbol();
+    // }
+
+    function testTotalSupply() public {
+        erc20.totalSupply();
     }
 }
